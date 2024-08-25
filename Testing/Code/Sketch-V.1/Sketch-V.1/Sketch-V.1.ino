f766e09404c64c0e1d5f4ba982b31179c8cfb85a
Jolomolokolo <93781011+Jolomolokolo@users.noreply.github.com>
2024-08-25 18:58:53 +0200

    Create Sketch-V.1.ino

---
+++ Testing/Code/Sketch-V.1/Sketch-V.1/Sketch-V.1.ino
@@ -0,0 +1,26 @@
+const int NUM_SLIDERS = 5;
+const int analogInputs[NUM_SLIDERS] = {A0, A1, A2, A3, A4};
+
+int analogSliderValues[NUM_SLIDERS];
+
+void setup() { 
+  for (int i = 0; i < NUM_SLIDERS; i++) {
+    pinMode(analogInputs[i], INPUT);
+  }
+
+  Serial.begin(9600);
+}
+
+void loop() {
+  updateSliderValues();
+  sendSliderValues(); // Actually send data (all the time)
+  // printSliderValues(); // For debug
+  delay(10);
+}
+
+void updateSliderValues() {
+  for (int i = 0; i < NUM_SLIDERS; i++) {
+     analogSliderValues[i] = analogRead(analogInputs[i]);
+  }
+}
+
